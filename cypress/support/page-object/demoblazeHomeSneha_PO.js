7b323bf3460a26b2687979409ca1f07f6590d87c
Sneha Ghanekar <snehaghanekar@Snehas-MacBook-Pro.local>
2024-02-12 20:52:16 -0800

    renamed files multiple files commited..

---
+++ cypress/support/page-object/demoblazeHomeSneha_PO.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+
+//                                   
+  
+
+  /// <reference types="cypress" />
+
+class demoblazeHome {
+  constructor() {
+    this.loginButton = "#login2"
+    this.usernameInput = "#loginusername";
+    this.passwordInput = "#loginpassword";
+    this.loginButtonModal = "button[onclick='logIn()']"
+  }
+
+  loginValid() {
+    cy.fixture("demoblaze.json").then((data) => {
+      cy.visit(data.baseUrl);
+      cy.get(loginButton).click();
+      cy.get(usernameInput).type(data.username);
+      cy.get(passwordInput).type(data.password);
+      cy.get(loginButtonModal).click();
+    });
+  }
+}
+
+export default demoblaze_PO;
