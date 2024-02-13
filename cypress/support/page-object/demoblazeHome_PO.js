bb739d30cb9cde69dcb66b1625131d4695d93cdb
JaneBe <142463200+JaneBe@users.noreply.github.com>
2024-02-12 23:08:11 -0500

    Change 2.12

--- cypress/support/page-object/demoblazeHome_PO.js
+++ cypress/support/page-object/demoblazeHome_PO.js
@@ -1,11 +1,13 @@
 /// <reference types="cypress" />
 
-class demoblazeHome {
+class demoblazeHome_PO {
   constructor() {
     this.loginButton = "#login2";
     this.usernameInput = "#loginusername";
     this.passwordInput = "#loginpassword";
     this.loginButtonModal = "button[onclick='logIn()']";
+    this.header = "#navbarExample";
+    this.footer = "#fotcont";
   }
 
   loginValid() {
@@ -15,8 +17,10 @@ class demoblazeHome {
       cy.get(this.usernameInput).type(data.username);
       cy.get(this.passwordInput).type(data.password);
       cy.get(this.loginButtonModal).click();
+      cy.get(this.header).should("have.text",data.headerText);
+      cy.get(this.footer).should("have.text",data.footerText);
     });
   }
 }
 
-export default demoblazeHome;
+export default demoblazeHome_PO;
