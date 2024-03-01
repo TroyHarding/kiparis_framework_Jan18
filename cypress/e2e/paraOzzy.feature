a919598e89749865a1942062363fec31f4d21199
Troy Harding <ozzyqa@gmail.com>
2024-02-29 20:15:25 -0800

    Register case

--- cypress/e2e/paraOzzy.feature
+++ cypress/e2e/paraOzzy.feature
@@ -1,23 +1,22 @@
 @para @paraOzzy
 Feature: ParaBank
 
-    Feature Description
+  @para1 @paraOzzy1
+  Scenario: Register
+    Given I navigate to the default parabank page
+    When I click the Register button
+    When I fill out all of the required registration fields
+    When I click the Register button to complete sign up
+    Then I veriy my account has been created
+  #Login with the newly created user and Logout
+
+
+  @para2 @paraOzzy2
+  Scenario: Open New Account
 
 
 
-    @para1 @paraOzzy1
-    Scenario: Register
 
 
-
-
-
-      @para2 @paraOzzy2
-    Scenario: Open New Account
-
-
-
-
-
-      @para2 @paraOzzy2
-    Scenario: Tranfer Funds
+  @para2 @paraOzzy2
+  Scenario: Tranfer Funds
--- cypress/support/step_definitions/Para_steps.js
+++ cypress/support/step_definitions/Para_steps.js
@@ -3,9 +3,45 @@ import {
     Given,
     When,
     Then,
-    And,
   } from "@badeball/cypress-cucumber-preprocessor";
   import "cypress-iframe";
 
 
-  
+  Given(`I navigate to the default parabank page`, () => {
+    cy.fixture("parabank_ozzy.json").then((data) => {
+      cy.visit(data.baseUrl);
+    });
+  });
+
+  When(`I click the Register button`, () => {
+    cy.fixture("parabank_ozzy.json").then((data) => {
+      cy.get(data.registerHome).click();
+    });
+  });
+
+  When(`I fill out all of the required registration fields`, () => {
+    cy.fixture("parabank_ozzy.json").then((data) => {
+      cy.get(data.firstName).type(data.registerUser.firstName);
+      cy.get(data.lastName).type(data.registerUser.lastName);
+      cy.get(data.address).type(data.registerUser.address);
+      cy.get(data.city).type(data.registerUser.city);
+      cy.get(data.state).type(data.registerUser.state);
+      cy.get(data.zipCode).type(data.registerUser.zipCode);
+      cy.get(data.phone).type(data.registerUser.phone);
+      cy.get(data.ssn).type(data.registerUser.ssn);
+      cy.get(data.userName).type(data.registerUser.userName);
+      cy.get(data.password).type(data.registerUser.password);
+      cy.get(data.confirmPassword).type(data.registerUser.password);
+
+      cy.get(data.registerForm).click();
+
+    });
+  });
+
+  When(`I click the Register button to complete sign up`, () => {
+   
+  });
+
+  Then(`I veriy my account has been created`, () => {
+   
+  });
